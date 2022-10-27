65cba156e09b517033142ff843e685fee1a74823
Euncheol-s <62688540+Euncheol-s@users.noreply.github.com>
2022-10-27 20:30:42 +0900

    Update FIles
    내비게이션 애니메이션 구현, 카드 리스트 페이지 구현, 카드 보여주는 애니메이션 구현 필요

--- javascript/clickNav.js
+++ javascript/clickNav.js
@@ -1,14 +1,25 @@
 const navBtn = document.getElementById("navBtn");
 const navBar = document.getElementById("navBar");
+const link = document.querySelector("#navBar a");
+
 const onCLick = navBtn.addEventListener("click", handleClickBtn);
+
 let is_spread = false;
 
 function handleClickBtn(event) {
-    if(!is_spread) {
-        navBar.classList.remove("hidden");
-        is_spread = true;
-    }else {
-        navBar.classList.add("hidden");
-        is_spread = false;
-    }
+  if (!is_spread) {
+    navBar.classList.remove("hidden");
+    navBar.classList.remove("shorten");
+    navBar.classList.add("widen");
+    console.log(navBar.classList);
+    is_spread = true;
+  } else {
+    navBar.classList.remove("widen");
+    navBar.classList.add("shorten");
+    setTimeout(function () {
+      navBar.classList.add("hidden");
+    }, 1000);
+    console.log(navBar.classList);
+    is_spread = false;
+  }
 }
