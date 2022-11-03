ee5189e3ee0ba815dd07159423386495ca2740a3
yerimYeon <99879845+yinneu@users.noreply.github.com>
2022-11-03 16:40:51 +0900

    FrontEnd: Listcard
    리스트 카드 페이지 수정

---
+++ config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  host: "localhost",
+  user: "root",
+  password: "rkskek",
+  database: "Random_mj",
+};
+
+// module.exports = {
+//   host: "3.36.229.145",
+//   user: "root",
+//   password: "rkskek204",
+//   database: "Random_mj",
+// };
