646fb95b29646f8af73372d1eee40f3f7a419599
yerimYeon <99879845+yinneu@users.noreply.github.com>
2022-11-16 08:09:28 +0900

    frontend :
    1. 메인페이지
    0 미디어쿼리: 상세페이지 사이즈 변경
    0  미디어쿼리: 상세페이지 slide up 애니메이션 구현
    0 모바일 네비 사라짐 수정 + 위치 에러해결 => nav fixed로 변경
    0 모바일 터치 스크롤시 카드로테이트 구현
    
    2. 추가적인거
    0 css 코드 정리
    0 카드 close 아이콘 변경

--- javascript/cardRotate.js
+++ javascript/cardRotate.js
@@ -4,64 +4,62 @@ const ANGLE = 360 / cards.length;
 let onHover = false;
 let angle_after = 0;
 
-const media_mobile_vertical = matchMedia(
-  "(max-width: 786px)"
-);
+// 카드 원형으로 배치
+for (let i = 0; i < cards.length; i++) {
+  cards[i].style.transform = `
+    rotate(${ANGLE * i}deg) translate(600px) rotate(90deg)`;
+}
+for (let i = 0; i < cards.length; i++) {
+  cards[i].addEventListener("mouseenter", cardHover);
+  cards[i].addEventListener("mouseleave", cardOut);
+}
 
-//랩탑 or 데스크탑일 때 카드 배치
-// if (!media_mobile_vertical.matches) {
-  for (let i = 0; i < cards.length; i++) {
-    cards[i].style.transform = `
-      rotate(${ANGLE * i}deg) translate(600px) rotate(90deg)`;
+function cardHover(event) {
+  event.target.classList.add("cardScale");
+}
+function cardOut(event) {
+  event.target.classList.remove("cardScale");
+}
+
+
+// 마우스 휠 작동시 카드 로테이트
+window.addEventListener("mousewheel", handleScroll);
+function handleScroll(event) {
+  let direction = event.wheelDelta;
+  if (direction > 0) {
+    angle_after -= 35;
+  } else {
+    angle_after += 35;
   }
-  for (let i = 0; i < cards.length; i++) {
-    cards[i].addEventListener("mouseenter", cardHover);
-    cards[i].addEventListener("mouseleave", cardOut);
+  // 상세페이지 close 에만 rotate
+  if(!isOpen){
+    cardArea.style.transform = "rotate(" + angle_after + "deg";
   }
+}
 
-  window.addEventListener("mousewheel", handleScroll);
 
-  function cardHover(event) {
-    event.target.classList.add("cardScale");
+// 터치 스크롤시 카드 로테이트
+let startx, starty, endx, endy;
+window.addEventListener("touchstart", (event) => {
+  startx = event.changedTouches[0].screenX;
+  starty = event.changedTouches[0].screenY;
+})
+window.addEventListener("touchend", (event) => {
+  endx = event.changedTouches[0].screenX;
+  endy = event.changedTouches[0].screenY;
+
+  let rightRotate = starty-endy;
+  let leftRotate = endy-starty;
+
+  if(rightRotate > 30){
+    angle_after -= 100;
+  }else if(leftRotate > 30){
+    angle_after += 100;
   }
-  function cardOut(event) {
-    event.target.classList.remove("cardScale");
+  if(!isOpen){
+    cardArea.style.transform = "rotate(" + angle_after + "deg";
   }
+})
 
 
-  function handleScroll(event) {
-    let direction = event.wheelDelta;
-    // if (onHover == true) {
-      if (direction > 0) {
-        angle_after -= 35;
-      } else {
-        angle_after += 35;
-      }
-      cardArea.style.transform = "rotate(" + angle_after + "deg";
-    // }
-  }
-// }
 
-// 모바일 랜덤 카드 리스트
-// else {
-//   for (let i = 0; i < cards.length; i++) {
-//     cards[i].style.transform = `
-//       rotate(${ANGLE * i}deg) translate(400px) rotate(90deg)`;
-//   }
-//   for (let i = 0; i < cards.length; i++) {
-//     cards[i].addEventListener("mouseenter", cardHover);
-//     cards[i].addEventListener("mouseleave", cardOut);
-//   }
-
-//   cardArea.addEventListener("mousewheel", handleScroll);
-
-//   function handleScroll(event) {
-//     let direction = event.wheelDelta;
-//     if (direction > 0) {
-//       angle_after -= 35;
-//     } else {
-//       angle_after += 35;
-//     }
-//     cardArea.style.transform = "rotate(" + angle_after + "deg";
-//   }
-//}
--- javascript/floating.js
+++ javascript/floating.js
@@ -1,3 +1,4 @@
+// Background icon floating
 const foods = document.getElementById("iconArea").children;
 const positionX = [4, 16, 35, 45, 54, 61, 61, 74, 76, 86, 91];
 const positionY = [49, 78, 17, 90, 36, 11, 71, 38, 94, 63, 25];
